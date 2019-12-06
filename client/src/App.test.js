717689c42a41b4ecec23cf53aaea94d4c41879bc
Emily <bruner.emilym@gmail.com>
2019-12-06 11:25:54 -0500

    custom hook implemented

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import { PlayerCard } from './components/PlayerCard';
 
 test('renders without crashing', () => {
   render(<App />);
 });
 
-test('Get homepage title', () => {
-  const { getByText } = render(<App />);
-  getByText(/Women's World Cup Players/i);
+test('toggle loads dark mode', () => {
+  const { getByTestId } = render(<App />);
+  getByTestId("toggle-element");
+});
+
+test('Players are defined', () => {
+  const players = <PlayerCard />;
+  expect(players).toBeDefined();
+  expect(players).not.toBeUndefined();
+  expect(players).not.toBeFalsy;
 });
